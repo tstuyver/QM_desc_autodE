0b6d9d74fdf0b95144ed1645763efeed9a06345d
tstuyver <thijsstuyver@hotmail.com>
2022-03-16 19:26:42 -0400

    Delete individual folders upon xyz-file extraction

--- get_xyz_files.py
+++ get_xyz_files.py
@@ -3,21 +3,21 @@ import tarfile
 import os
 import subprocess
 
-
 parser = ArgumentParser()
-parser.add_argument('--tar_file', type=str, required=True,
-                    help='tar file to extract .xyz files from')
+parser.add_argument(
+    "--tar_file", type=str, required=True, help="tar file to extract .xyz files from"
+)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parser.parse_args()
-
-    if not os.path.isdir('xyz_files'):
-        os.mkdir('xyz_files')
-    path = os.path.join(os.getcwd(), 'xyz_files')
+    if not os.path.isdir("xyz_files"):
+        os.mkdir("xyz_files")
+    path = os.path.join(os.getcwd(), "xyz_files")
     tar = tarfile.open(args.tar_file)
-    os.chdir('/')
+    os.chdir("/")
     tar.extractall()
     tar.close()
     os.chdir(path)
-    subprocess.run(['rm','-r','conformers/'])
+    dir_list = [path for path in os.listdir() if os.path.isdir(path)]
+    for dir in dir_list:
+        subprocess.run(["rm", "-r", f"{dir}"])
