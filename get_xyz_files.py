02d6d6403ee4b3f76594368cd21df52f51142d22
tstuyver <thijsstuyver@hotmail.com>
2022-08-29 15:47:01 -0400

    Include documentation + black reformatting

--- get_xyz_files.py
+++ get_xyz_files.py
@@ -7,11 +7,16 @@ parser = ArgumentParser()
 parser.add_argument(
     "--tar-file", type=str, required=True, help="tar file to extract .xyz files from"
 )
-parser.add_argument("--folder-name", type=str, default="XTB_opt", help="name of the folder to which the tar-archive is extracted")
+parser.add_argument(
+    "--folder-name",
+    type=str,
+    default="XTB_opt",
+    help="name of the folder to which the tar-archive is extracted",
+)
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    #if not os.path.isdir("xyz_files"):
+    # if not os.path.isdir("xyz_files"):
     #    os.mkdir("xyz_files")
     path = os.path.join(os.getcwd(), "xyz_files")
     path_tar = os.path.join(os.getcwd(), args.tar_file)
@@ -20,7 +25,7 @@ if __name__ == "__main__":
     os.chdir("/")
     tar.extractall()
     tar.close()
-    #os.chdir(path_folder)
+    # os.chdir(path_folder)
     dir_list = [item for item in os.listdir(path_folder) if os.path.isdir(item)]
     for dir in dir_list:
         subprocess.run(["rm", "-r", f"{dir}"])
