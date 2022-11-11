9a4ac317e357eb5e827f43f45d1b2d8701d03166
tstuyver <thijsstuyver@hotmail.com>
2022-11-11 12:25:17 -0500

    Clean up submit.sh file

--- submit.sh
+++ submit.sh
@@ -1,22 +1,20 @@
-#!/bin/bash 
-#SBATCH -N 1 
+#!/bin/bash
+#SBATCH -N 1
 #SBATCH -n 48
-#SBATCH --time=50:59:00 
-#SBATCH --mem 192000 
-#SBATCH --output=6416
+#SBATCH --time=99:59:00
+#SBATCH --constraint=centos7
+#SBATCH --partition=sched_mit_ccoley
+#SBATCH --nodelist node1238
+#SBATCH --mem 192000
+#SBATCH --output=dipoles
 
-source /home/gridsan/tstuyver/.bashrc 
-eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+source /home/tstuyver/.bashrc
 conda activate QM_descriptors
-export g16root=/home/gridsan/groups/RMG/Software/gaussian/
-export PATH=$g16root/g16/:$g16root/gv:$PATH
-GAUSS_SCRDIR=~/scratch/gaussian//$SLURM_JOB_NAME-$SLURM_JOB_ID 
-. $g16root/g16/bsd/g16.profile
-mkdir -p $GAUSS_SCRDIR   
-chmod 750 $GAUSS_SCRDIR  
- 
-python main.py --ismiles scope6416smiles.csv --xtb_folder xtb_6416 --DFT_folder DFT_6416
+module load gaussian/16.c01
+GAUSS_SCRDIR=/nobackup1/tstuyver//$SLURM_JOB_NAME-$SLURM_JOB_ID
+mkdir -p $GAUSS_SCRDIR
+chmod 750 $GAUSS_SCRDIR
 
-echo "jobs done, removing scratch folder: $(date)"
+python main.py --ismiles dipoles.csv --DFT-folder dipoles_DFT --xtb-folder dipoles_XTB_opt --resume-fail
+
 rm -rf $GAUSS_SCRDIR
-
